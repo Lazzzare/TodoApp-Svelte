9722c834bec0bf010f7b6eec54e2a75770b7c6ba
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-10 16:48:07 +0400

    import svelte files :apple:

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+  import TitleContent from "./lib/TitleContent/TitleContent.svelte";
+  import TodoForm from "./lib/TodoContent/TodoForm.svelte";
+  import TodoHeader from "./lib/TodoContent/TodoHeader.svelte";
+  import TodoList from "./lib/TodoContent/TodoList.svelte";
+
 
 </script>
 
-<main>
-
+<main class="w-full h-screen bg-[#EEE] flex flex-row justify-center items-center">
+<TitleContent title={'Todo'}/>
+<div>
+    <TodoHeader />
+    <TodoForm />
+    <TodoList />
+</div>
 </main>
