897e6b75ab8810f41e2f033363c9c5205743f54a
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-10 17:07:30 +0400

    Update TodoHeader.svelte

---
+++ src/lib/TodoContent/TodoHeader.svelte
@@ -0,0 +1,12 @@
+<script>
+    import TodoHeaderImage from '../../assets/TodoHeaderImage.jpg'
+</script>
+
+<div class="relative">
+    <img src={TodoHeaderImage} alt="TodoHeaderImage"
+    class="w-[430px] h-[202px] rounded-t-[10px]">
+    <div class="absolute font-russoOne text-white right-7 bottom-3">
+        <span class="text-lg">Thur 9</span>
+        <h1 class="text-5xl">6:23 AM</h1>
+    </div>
+</div>
