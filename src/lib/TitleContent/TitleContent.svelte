f8b490edb33ededac42f35d7141f737cd25bc751
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-11 15:28:34 +0400

    Update TitleContent.svelte

--- src/lib/TitleContent/TitleContent.svelte
+++ src/lib/TitleContent/TitleContent.svelte
@@ -2,6 +2,8 @@
   export let title: string;
 </script>
 
-<h1 class="font-inter text-[#007FDB] text-8xl font-bold text-center mb-4">
+<h1
+  class="hidden md:flex font-inter text-[#007FDB] text-8xl font-bold text-center mb-4"
+>
   {title}
 </h1>
